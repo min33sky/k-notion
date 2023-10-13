271f1a46e9e578fbff777cb91234f025c6528b67
min33sky <min33sky@naver.com>
2023-10-13 13:28:06 +0900

    feat: auth

---
+++ convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://noble-goblin-60.clerk.accounts.dev/',
+      applicationID: 'convex',
+    },
+  ],
+};
--- src/app/(marketing)/_components/navbar.tsx
+++ src/app/(marketing)/_components/navbar.tsx
@@ -4,8 +4,14 @@ import { ThemeToggle } from '@/components/theme-toggle';
 import { useScrollTop } from '@/hooks/use-scroll-top';
 import { cn } from '@/lib/utils';
 import Logo from './logo';
+import { useConvexAuth } from 'convex/react';
+import Spinner from '@/components/spinner';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
 
 export default function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +23,28 @@ export default function Navbar() {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading ? <Spinner /> : null}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton mode="modal">
+              <Button variant="ghost" size="sm">
+                Log in
+              </Button>
+            </SignInButton>
+            <SignInButton mode="modal">
+              <Button size="sm">Get Jotion free</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">Enter Jotion</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
+
         <ThemeToggle />
       </div>
     </div>
