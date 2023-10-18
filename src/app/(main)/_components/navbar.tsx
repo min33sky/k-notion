16adf0867d2ac6d56edf032dee5ffaa584bdefe5
min33sky <min33sky@naver.com>
2023-10-18 22:15:26 +0900

    feat: publish

--- src/app/(main)/_components/navbar.tsx
+++ src/app/(main)/_components/navbar.tsx
@@ -8,6 +8,7 @@ import { Id } from '../../../../convex/_generated/dataModel';
 import Title from './title';
 import Menu from './menu';
 import Banner from './banner';
+import Publish from './publish';
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -49,6 +50,7 @@ export default function Navbar({ isCollapsed, onResetWidth }: NavbarProps) {
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
           <div className="flex items-center gap-x-2">
+            <Publish initialData={document} />
             <Menu documentId={document._id} />
           </div>
         </div>
